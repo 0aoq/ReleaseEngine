5f6946dc24ca93177a55487b81368db4e1a7e99b
0aoq <a0oq@outlook.com>
2021-11-26 16:58:16 -0500

    Fix test

--- test/import.js
+++ test/import.js
@@ -1,5 +1,5 @@
 // ReleaseEngine uses RequireJS by default to require javascript files
-require(["../package/compiled/ReleaseEngine.js"], function(engine) {
+require(["https://re-compiled.vercel.app/ReleaseEngine.js"], function(engine) {
     engine.newFile("./file.js", "javascript")
     engine.newFile("./file.html", "html", { placement: "start" })
     engine.newFile("./file.html", "html", { placement: "beforeend" })
