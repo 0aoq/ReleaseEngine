be638d56edc7bb288ec482c829f3e0508c76d2e3
0aoq <a0oq@outlook.com>
2021-11-27 21:19:51 -0500

    Content fixes

--- package/source/ReleaseEngine.ts
+++ package/source/ReleaseEngine.ts
@@ -1,3 +1,4 @@
+let __url = ""
 let replacements = [
     { start: '"test loaded"', end: 'console.log("Loaded document.")' },
     { start: '!;', end: '!important;' }
@@ -6,6 +7,16 @@ let replacements = [
 // files--
 type BlobType = 'text/javascript' | 'text/css' | 'text/html' | 'text/plaintext'
 const file = {
+    RunReplacements: (fetchedDocument) => {
+        for (let replacement of replacements) { // replace the data
+            fetchedDocument = fetchedDocument.replaceAll(replacement.start, replacement.end)
+        }; return fetchedDocument
+    },
+    DetermineFinalPath: (path: string) => {
+        let finalUrl = path
+        if (!finalUrl.startsWith("https://")) { finalUrl = `${__url}/${path}` }
+        return finalUrl
+    },
     Blobify: (data: string, type: BlobType) => {
         // create a blob url
         const blob = new Blob([data], { type })
@@ -15,50 +26,62 @@ const file = {
         // fetch the file and return it to the caller
         fetch(path)
             .then(response => response.text())
-            .then((data: any) => { callback(data) })
+            .then(data => callback(data))
     },
     LoadFile: function (path: string, type: BlobType, extraOptions: any = { placement: "beforeend", doCallstack: true }) {
         file.GetFile(path, (fetchedDocument) => {
             extraOptions.doCallstack = extraOptions.doCallstack ?? true
-
-            for (let replacement of replacements) { // replace the data
-                fetchedDocument = fetchedDocument.replaceAll(replacement.start, replacement.end)
-            }
-
+            fetchedDocument = file.RunReplacements(fetchedDocument)
             fetchedDocument.split(/\n?\r/).forEach(ln => {
                 // base for including a "path: ../PATH/IMPORT" in files, and causing them to be indexed too
                 let spl = ln.split('"path: ')
                 if (spl[1]) { spl = spl[1] } else { return }; spl = spl.split('"')[0]
-                if (spl === undefined) { return }; file.LoadFile(spl, type, extraOptions)
-                if (extraOptions.doCallstack) {
-                    console.log("ReleaseEngine Callstack:"); console.table({ caller: path, called: spl })
+                if (spl !== undefined) {
+                    if (extraOptions.doCallstack) {
+                        console.log("ReleaseEngine Callstack:"); console.table({ caller: path, called: spl })
+                    }
+
+                    let __fileUrl = ""
+                    file.GetFile(file.DetermineFinalPath(spl), (temp_file) => {
+                        temp_file = file.RunReplacements(temp_file); __fileUrl = file.Blobify(temp_file, type)
+                        fetchedDocument = fetchedDocument.replaceAll(
+                            ln,
+                            ln.replaceAll(`"path: ${spl}"`, `"${__fileUrl}"`)
+                        ); file.LoadFile(__fileUrl, type, extraOptions)
+                    })
                 }
             })
 
-            const fileUrl = file.Blobify(fetchedDocument, type) // create a url
-            switch (type) { // determine what to import and how to import it
-                case "text/css": // stylesheets
-                    document.head.insertAdjacentHTML("beforeend", `<link rel="stylesheet" href="${fileUrl}" class="__ReleaseEngine">`); break
-                case "text/html": // html markup
-                    if (extraOptions.placement === "start") {
-                        document.body.innerHTML = `<document><!-- alt: ${fileUrl} -->\n${fetchedDocument}</document>` + document.body.innerHTML
-                    } else {
-                        document.body.insertAdjacentHTML(extraOptions.placement ?? "beforeend", `
+            setTimeout(() => {
+                const fileUrl = file.Blobify(fetchedDocument, type) // create a url
+                window.sessionStorage.clear() // clear the session storage
+                window.sessionStorage.setItem(path, fileUrl) // store the url in session storage
+                switch (type) { // determine what to import and how to import it
+                    case "text/css": // stylesheets
+                        document.head.insertAdjacentHTML("beforeend", `<link rel="stylesheet" href="${fileUrl}" class="__ReleaseEngine">`); break
+                    case "text/html": // html markup
+                        if (extraOptions.placement === "start") {
+                            document.body.innerHTML = `<document><!-- alt: ${fileUrl} -->\n${fetchedDocument}</document>` + document.body.innerHTML
+                        } else {
+                            document.body.insertAdjacentHTML(extraOptions.placement ?? "beforeend", `
                         <document><!-- alt: ${fileUrl} -->\n${fetchedDocument}</document>`)
-                    }; break
-                case "text/javascript": // javascript files
-                    require([fileUrl], function ($) {
-                        $.main() // run the file's main function once
-                        if ($.update) {
-                            setInterval($.update, 1) // run the file's update function every frame (0.1s)
-                        }
-                    })
+                        }; break
+                    case "text/javascript": // javascript files
+                        require([fileUrl], function ($) {
+                            $.main(__url) // run the file's main function once
+                            if ($.update) {
+                                setInterval($.update, 1) // run the file's update function every frame (0.1s)
+                            }
+                        })
 
-                    break
-                default: // non-standard type
-                    document.head.insertAdjacentHTML("beforeend", `
+                        break
+                    default: // non-standard type
+                        document.head.insertAdjacentHTML("beforeend", `
                     <script type="${type}" class="__ReleaseEngine"><!-- alt: ${fileUrl} -->\n${fetchedDocument}</script>`); break
-            }
+                }
+
+                return fileUrl
+            }, 100);
         })
     }
 }
@@ -81,14 +104,18 @@ export const newFile = (path: string, type: DefineType, extraOptions: any) => {
     }
 
     // add file
-    file.LoadFile(path, finalType, extraOptions); return 1
+    file.LoadFile(file.DetermineFinalPath(path), finalType, extraOptions); return 1
 }
 
 export const createReplacement = (start: string, end: string) => {
     replacements.push({ start: start, end: end })
 }
 
-export default { newFile, createReplacement }
+export const setUrl = (url: string) => {
+    __url = url
+}
+
+export default { newFile, createReplacement, setUrl }
 
 // clean document
 setInterval(() => {
--- test/import.js
+++ test/import.js
@@ -1,6 +1,7 @@
 // ReleaseEngine uses RequireJS by default to require javascript files
 require(["https://re-compiled.vercel.app/ReleaseEngine.js"], function(engine) {
-    engine.newFile("./file.js", "javascript", { doCallstack: false })
-    engine.newFile("./file.html", "html", { placement: "start", doCallstack: false })
-    engine.newFile("./file.html", "html", { placement: "beforeend", doCallstack: false })
+    engine.setUrl("./")
+    engine.newFile("file.js", "javascript", { doCallstack: false })
+    engine.newFile("file.html", "html", { placement: "start", doCallstack: false })
+    engine.newFile("file.html", "html", { placement: "beforeend", doCallstack: false })
 })
